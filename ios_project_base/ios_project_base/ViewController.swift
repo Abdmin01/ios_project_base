fcc2594a91ad47288c37ed1282d343cef126f669
Mukmin <mukmin@terato.com>
2018-10-10 13:00:30 +0800

    First commit

---
+++ ios_project_base/ios_project_base/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_project_base
+//
+//  Created by Mukmin on 10/10/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
