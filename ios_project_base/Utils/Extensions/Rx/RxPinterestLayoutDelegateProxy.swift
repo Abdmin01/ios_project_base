a2df4bfff35ccac34fa99747e7ce3a30af60ba15
Abdmin <mukmin@terato.com>
2022-10-04 17:18:15 +0800

    [FIX] Import UIKit

--- ios_project_base/Utils/Extensions/Rx/RxPinterestLayoutDelegateProxy.swift
+++ ios_project_base/Utils/Extensions/Rx/RxPinterestLayoutDelegateProxy.swift
@@ -8,6 +8,7 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import UIKit
 
 class RxPinterestLayoutDelegateProxy: DelegateProxy<PinterestLayout, PinterestLayoutDelegate>, PinterestLayoutDelegate, DelegateProxyType {
 
